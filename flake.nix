5fc08832acb2558d9e813a1f81d9846f5554379f
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-10-25 17:00:09 +0200

    Add clippy

--- flake.nix
+++ flake.nix
@@ -16,6 +16,7 @@
           devShells.default = pkgs.mkShell {
             nativeBuildInputs = with pkgs; [
               cargo
+              clippy
               rustc
               rustc.llvmPackages.lld
               wasm-pack
