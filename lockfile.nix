efcd5c34c0f3b1a9b105df023e61759d0ee65757
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2024-11-17 21:14:44 +0100

    cargo: update deps (#44)

--- lockfile.nix
+++ lockfile.nix
@@ -1,4 +1,4 @@
 {
   lockFile = ./Cargo.lock;
-  outputHashes."tree-sitter-nix-0.0.1" = "sha256-eY+e48XFHvTRlew4LvndGHQog3d/RKDLlIrGr7QOWBY=";
+  outputHashes."tree-sitter-nix-0.0.1" = "sha256-Vi+hdH6gAAXsqOsTjEzzwU2TvpBqxO4Jxfa7omUtJ5o=";
 }
