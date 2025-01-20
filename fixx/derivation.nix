a1176e2a10ce745ff8f63e4af124ece8fe0b1648
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2025-01-20 12:13:54 +0100

    misc: update deps (#46)

--- fixx/derivation.nix
+++ fixx/derivation.nix
@@ -1,10 +1,10 @@
 { lib, rustPlatform, binaryen, rustc, wasm-pack, wasm-bindgen-cli }:
 
 let
-  wasm-bindgen-95 = wasm-bindgen-cli.override {
-    version = "0.2.95";
-    hash = "sha256-prMIreQeAcbJ8/g3+pMp1Wp9H5u+xLqxRxL+34hICss=";
-    cargoHash = "sha256-6iMebkD7FQvixlmghGGIvpdGwFNLfnUcFke/Rg8nPK4=";
+  wasm-bindgen-100 = wasm-bindgen-cli.override {
+    version = "0.2.100";
+    hash = "sha256-3RJzK7mkYFrs7C/WkhW9Rr4LdP5ofb2FdYGz1P7Uxog=";
+    cargoHash = "sha256-tD0OY2PounRqsRiFh8Js5nyknQ809ZcHMvCOLrvYHRE=";
   };
   manifest = (lib.importTOML ./Cargo.toml).package;
 in
@@ -19,7 +19,7 @@ rustPlatform.buildRustPackage rec {
     binaryen
     rustc.llvmPackages.lld
     wasm-pack
-    wasm-bindgen-95
+    wasm-bindgen-100
   ];
 
   buildPhase = ''
