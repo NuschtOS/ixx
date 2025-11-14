904eac14963b3b437f5bb497dd082182069a834f
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-11-14 01:05:25 +0100

    Fix nix packages build on stable

--- flake.nix
+++ flake.nix
@@ -12,6 +12,7 @@
           pkgs = (import nixpkgs) {
             inherit system;
           };
+          inherit (pkgs) lib;
         in
         {
           devShells.default = pkgs.mkShell {
@@ -27,10 +28,15 @@
             RUST_SRC_PATH = pkgs.rust.packages.stable.rustPlatform.rustLibSrc;
           };
 
-          packages = {
-            ixx = pkgs.callPackage ./ixx/derivation.nix { };
-            fixx = pkgs.callPackage ./fixx/derivation.nix { };
-            libixx = pkgs.callPackage ./libixx/derivation.nix { };
+          packages = let
+            rustPlatform = if lib.versionAtLeast pkgs.rustc.version "1.88" then
+              pkgs.rustPlatform
+            else
+              pkgs.rustPackages_1_89.rustPlatform;
+          in {
+            ixx = pkgs.callPackage ./ixx/derivation.nix { inherit rustPlatform; };
+            fixx = pkgs.callPackage ./fixx/derivation.nix { inherit rustPlatform; };
+            libixx = pkgs.callPackage ./libixx/derivation.nix { inherit rustPlatform; };
           };
         }
       );
