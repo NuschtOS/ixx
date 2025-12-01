a3344268e9420a2890646ce4019de9751d5da7f3
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-12-01 21:49:59 +0100

    Do not write empty json fields

--- libixx/src/package.rs
+++ libixx/src/package.rs
@@ -5,22 +5,40 @@ use url::Url;
 #[serde(deny_unknown_fields, rename_all = "camelCase")]
 pub struct Package {
   pub attr_name: String,
+  #[serde(skip_serializing_if = "Option::is_none")]
   pub broken: Option<bool>,
+  #[serde(skip_serializing_if = "Option::is_none")]
   pub cpe: Option<String>,
+  #[serde(skip_serializing_if = "Option::is_none")]
   pub disabled: Option<bool>,
+  #[serde(skip_serializing_if = "Vec::is_empty")]
   pub possible_cpes: Vec<String>,
+  #[serde(skip_serializing_if = "Option::is_none")]
   pub purl: Option<String>,
+  #[serde(skip_serializing_if = "Option::is_none")]
   pub declaration: Option<Url>,
+  #[serde(skip_serializing_if = "Option::is_none")]
   pub description: Option<String>,
+  #[serde(skip_serializing_if = "Option::is_none")]
   pub long_description: Option<String>,
+  #[serde(skip_serializing_if = "Option::is_none")]
   pub eval_error: Option<bool>,
+  #[serde(skip_serializing_if = "Vec::is_empty")]
   pub homepages: Vec<Url>,
+  #[serde(skip_serializing_if = "Vec::is_empty")]
   pub known_vulnerabilities: Vec<String>,
+  #[serde(skip_serializing_if = "Vec::is_empty")]
   pub licenses: Vec<String>,
+  #[serde(skip_serializing_if = "Vec::is_empty")]
   pub maintainers: Vec<u32>,
+  #[serde(skip_serializing_if = "Option::is_none")]
   pub name: Option<String>,
+  #[serde(skip_serializing_if = "Vec::is_empty")]
   pub outputs: Vec<String>,
+  #[serde(skip_serializing_if = "Option::is_none")]
   pub pname: Option<String>,
+  #[serde(skip_serializing_if = "Vec::is_empty")]
   pub teams: Vec<String>,
+  #[serde(skip_serializing_if = "Option::is_none")]
   pub version: Option<String>,
 }
