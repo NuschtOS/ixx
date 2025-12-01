de65d15d2b420ccad8a23404fd7f1b3b5d736b88
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-12-01 13:45:19 +0100

    Make benchmarks easier to run

--- libixx/benches/search.rs
+++ libixx/benches/search.rs
@@ -3,7 +3,13 @@ use libixx::Index;
 use std::{fs::File, hint::black_box};
 
 fn criterion_benchmark(c: &mut Criterion) {
-  let mut file = File::open("../packages/index.ixx").unwrap();
+  let mut file = match File::open("../index.ixx") {
+    Ok(f) => f,
+    Err(e) => {
+      eprintln!("index.ixx is missing, you can download one from https://HEAD.nuschtos-search.pages.dev/data/packages/index.ixx and place it in the root of the project: {}", e);
+      std::process::exit(1);
+    }
+  };
   let index = Index::read_from(&mut file).unwrap();
 
   c.bench_function("search for zoo", |b| {
