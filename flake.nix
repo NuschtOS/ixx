c619cc93dd3b2699f96480f40d3f9904b2cb05e4
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-12-01 21:52:21 +0100

    Clean up 25.05 logic

--- flake.nix
+++ flake.nix
@@ -12,7 +12,6 @@
           pkgs = (import nixpkgs) {
             inherit system;
           };
-          inherit (pkgs) lib;
         in
         {
           devShells.default = pkgs.mkShell {
@@ -29,15 +28,10 @@
             RUST_SRC_PATH = pkgs.rust.packages.stable.rustPlatform.rustLibSrc;
           };
 
-          packages = let
-            rustPlatform = if lib.versionAtLeast pkgs.rustc.version "1.88" then
-              pkgs.rustPlatform
-            else
-              pkgs.rustPackages_1_89.rustPlatform;
-          in {
-            ixx = pkgs.callPackage ./ixx/derivation.nix { inherit rustPlatform; };
-            fixx = pkgs.callPackage ./fixx/derivation.nix { inherit rustPlatform; };
-            libixx = pkgs.callPackage ./libixx/derivation.nix { inherit rustPlatform; };
+          packages = {
+            ixx = pkgs.callPackage ./ixx/derivation.nix { };
+            fixx = pkgs.callPackage ./fixx/derivation.nix { };
+            libixx = pkgs.callPackage ./libixx/derivation.nix { };
           };
         }
       );
