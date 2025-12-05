3b0096726aaeb2db01e0e5dbf3e383790e48761f
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-12-05 01:30:39 +0100

    Add failing test

--- libixx/src/test/mod.rs
+++ libixx/src/test/mod.rs
@@ -14,6 +14,11 @@ fn test() {
   index_builder.push(0, "programs.home-manager.enable");
   index_builder.push(0, "services.home-manager.autoUpgrade.enable");
   index_builder.push(0, "services.home-manager.autoUpgrade.frequency");
+
+  index_builder.push(0, "pretalx");
+  index_builder.push(0, "nixosTests.pretalx");
+  index_builder.push(0, "nixosTests.allDrivers.pretalx");
+
   index_builder.push(1, "home.enableDebugInfo");
 
   let index: Index = index_builder.into();
@@ -69,7 +74,7 @@ fn test() {
     index.search(None, "ho*debug", 10).unwrap(),
     vec![
       (0, 0, "home.enableDebugInfo".to_string()),
-      (9, 1, "home.enableDebugInfo".to_string())
+      (12, 1, "home.enableDebugInfo".to_string())
     ]
   );
 
@@ -78,8 +83,27 @@ fn test() {
     Some(0)
   );
 
+  // Test ambiguous names: pretalx, nixosTests.pretalx, nixosTests.allDrivers.pretalx
+  assert_eq!(
+    index.get_idx_by_name(0, "pretalx").unwrap(),
+    Some(9),
+    "Should find 'pretalx' in scope 0"
+  );
+  assert_eq!(
+    index.get_idx_by_name(0, "nixosTests.pretalx").unwrap(),
+    Some(10),
+    "Should find 'nixosTests.pretalx' in scope 0"
+  );
+  assert_eq!(
+    index
+      .get_idx_by_name(0, "nixosTests.allDrivers.pretalx")
+      .unwrap(),
+    Some(11),
+    "Should find 'nixosTests.allDrivers.pretalx' in scope 0"
+  );
+
   assert_eq!(
     index.get_idx_by_name(1, "home.enableDebugInfo").unwrap(),
-    Some(9)
+    Some(12)
   );
 }
