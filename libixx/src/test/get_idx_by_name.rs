92c2ab16c497bf8b322f3b21c29f5e813582f41b
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-12-05 23:10:51 +0100

    Add very basic get_idx_by_name test, slim down test_get_idx_by_name_complex

--- libixx/src/test/get_idx_by_name.rs
+++ libixx/src/test/get_idx_by_name.rs
@@ -1,14 +1,38 @@
 use crate::{Index, IndexBuilder};
 
 #[test]
-fn test_get_idx_by_name() {
+fn test_get_idx_by_name_one_level() {
+  let mut index_builder = IndexBuilder::new(100);
+
+  index_builder.push(0, "a");
+  index_builder.push(0, "b");
+  index_builder.push(0, "c");
+
+  let index: Index = index_builder.into();
+
+  assert_eq!(
+    index.get_idx_by_name(0, "a").unwrap(),
+    Some(0),
+    "Should find 'a' in scope 0"
+  );
+  assert_eq!(
+    index.get_idx_by_name(0, "b").unwrap(),
+    Some(1),
+    "Should find 'b' in scope 0"
+  );
+  assert_eq!(
+    index.get_idx_by_name(0, "c").unwrap(),
+    Some(2),
+    "Should find 'c' in scope 0"
+  );
+}
+
+#[test]
+fn test_get_idx_by_name_complex() {
   let mut index_builder = IndexBuilder::new(100);
 
   index_builder.push(0, "home.enableDebugInfo");
   index_builder.push(0, "home.enableNixpkgsReleaseCheck");
-  index_builder.push(0, "programs.home-manager.enable");
-  index_builder.push(0, "services.home-manager.autoUpgrade.enable");
-  index_builder.push(0, "services.home-manager.autoUpgrade.frequency");
 
   index_builder.push(0, "pretalx");
   index_builder.push(0, "nixosTests.pretalx");
@@ -22,17 +46,17 @@ fn test_get_idx_by_name() {
 
   assert_eq!(
     index.get_idx_by_name(0, "pretalx").unwrap(),
-    Some(5),
+    Some(2),
     "Should find 'pretalx' in scope 0"
   );
   assert_eq!(
     index.get_idx_by_name(0, "nixosTests.pretalx").unwrap(),
-    Some(6),
+    Some(3),
     "Should find 'nixosTests.pretalx' in scope 0"
   );
   assert_eq!(
     index.get_idx_by_name(0, "nixosTests.allDrivers.pretalx").unwrap(),
-    Some(7),
+    Some(4),
     "Should find 'nixosTests.allDrivers.pretalx' in scope 0"
   );
 }
